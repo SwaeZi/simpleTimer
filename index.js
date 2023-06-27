9ea5beb6742c0d8b6d03ae93800a18d2fc9ec6d2
SwaeZi <stevenstefanov@icloud.com>
2023-06-27 10:31:50 +0200

    implement the refresh button, add in get and set func for time remaining

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
+  constructor(durationInput, startButton, pauseButton,refreshButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    this.refreshButton = refreshButton;
 
+    this.startButton.addEventListener("click", this.start.bind(this));
+    this.pauseButton.addEventListener("click", this.pause);
+    this.refreshButton.addEventListener("click", this.refresh);
+  }
 
-        this.startButton.addEventListener('click', this.start.bind(this));
-        this.pauseButton.addEventListener('click', this.pause);
-    }
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
 
-    start = () => {
-        this.tick(); 
-        this.interval = setInterval(this.tick, 1000);
+  pause = () => {
+    clearInterval(this.interval);
+  };
 
-    };
+  tick = () => {
+    this.timeRemaining = this.timeRemaining - 1;
+  };
 
-    tick = () => {
-        console.log("tick");      
-    };
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
 
-    pause = () => {
-        clearInterval(this.interval);
-    };
+  set timeRemaining(time) { 
+    this.durationInput.value = time;
+  }
 
-    
-
+  refresh = () => {
+    this.durationInput.value = 0;
+  }
 }
 
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const refreshButton = document.querySelector('#refresh');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton,refreshButton);
